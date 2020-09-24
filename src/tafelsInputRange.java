a58cec7f572570da648e81f25ac7cce2c3dff21b
Relativiteit <alejocain@gmail.com>
2020-09-24 10:32:59 +0200

    Grondgetal en range

---
+++ src/tafelsInputRange.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class tafelsInputRange{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num,range;
+        System.out.println("Voer een grond getal in: ");
+        num=sc.nextInt();
+        System.out.println("Wat is de gewenste range: ");
+        range=sc.nextInt();
+        for(int i=1; i<=range; i++){
+            System.out.println(num+"x"+i+"="+num*i);
+        }
+    }
+}
