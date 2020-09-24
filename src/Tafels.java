6d820e1cc808116bb19f051fdb6bb7409a6a7c21
Relativiteit <alejocain@gmail.com>
2020-09-24 10:16:39 +0200

    For loop om tafels te maken met input

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Tafels {
+
     public static void main(String[] args) {
-        System.out.println("Hello moto");
-        System.out.println("gaat het werken");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Voer getal in: ");
+        int n = in .nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + "*" + i + " = " + (n * i));
+        }
     }
 }
